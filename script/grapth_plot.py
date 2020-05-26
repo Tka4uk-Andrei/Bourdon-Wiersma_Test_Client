596441cdc8a12697fea525910f039558bf279718
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2020-05-26 14:33:47 +0300

    add graph printing process

---
+++ script/grapth_plot.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+
+
+def print_plot_for(time_arr, val_arr):
+    plt.plot(time_arr, val_arr, label = 'Показактель пользователя = Unknown') 
+    plt.xlabel('Время в секундах') 
+    plt.ylabel('Показатель коэффициента')
+    plt.title('Переключаемость внимания')
+    plt.legend()
+    plt.show() 
+
+
+if __name__ == '__main__':
+    val_arr = [1, 0.95, 0.8, 0.48, 0.60, 0.78, 0.97]
+    time_arr = [i * 10 for i in range(len(val_arr))]
+    print_plot_for(time_arr, val_arr)
